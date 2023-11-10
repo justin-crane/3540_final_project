7a307eb0641441d159abc3e1eea0874f1320056c
Marcus Blagdon <marcus.blagdon64@ed.cna.nl.ca>
2023-11-10 10:51:16 -0500

    -npm run build on frontend, moved to backend -backend ran npm install mongoose -updated server.js code to connect to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 // server/index.js
 
 const express = require("express");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3001;
-
+const MONGO_URI = 'mongodb://localhost:27017/VGTCDB';
 const app = express();
+
+// Connect to MongoDB
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.error('Could not connect to MongoDB:', err));
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
