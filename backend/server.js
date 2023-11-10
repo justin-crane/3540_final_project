91c10d181c211ed106c38c829c82bfa9d28503bd
philiptucker <philip.tucker37@ed.cna.nl.ca>
2023-11-10 18:57:30 -0500

    Added addGame page + post game to database

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,46 @@
 
 const express = require("express");
 const mongoose = require("mongoose");
+const multer = require("multer");
+const mongo = require("mongodb")
+const {MongoClient} = require("mongodb");
 
 const PORT = process.env.PORT || 3001;
-const MONGO_URI = 'mongodb://localhost:27017/VGTCDB';
+const MONGO_URI = 'mongodb+srv://PhilipT:5FMVbLHH2pUpdL6V@atlascluster.axuhh7n.mongodb.net/';
 const app = express();
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, '../frontend/public/images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+})
+
+const upload = multer({storage: storage});
+
 // Connect to MongoDB
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+/*mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected...'))
     .catch(err => console.error('Could not connect to MongoDB:', err));
+
+const db = mongoose.connection;*/
+
+app.post('/api/addGame', upload.single('file'), async (req, res) => {
+    const client = new MongoClient(MONGO_URI)
+    await client.connect();
+    const db = client.db("game-db")
+    await db.collection("gamelist").insertOne({
+        "name":req.body.name,
+        "console":req.body.console,
+        "img":req.body.img,
+        "condition":req.body.condition,
+        "availability":req.body.availability,
+        "notes":req.body.notes
+    });
+    await db.collection("gamelist").find({}).toArray();
+})
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
